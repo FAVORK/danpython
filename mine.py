33e0d5c60765be53b91af134099863014dd6fd5a
FAVORK <kamaukdan@gmail.com>
2020-01-22 19:22:19 +0300

    first commit

---
+++ mine.py
@@ -0,0 +1,13 @@
+class PlayerCharacter:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        print('run')
+
+player1 = PlayerCharacter('Cindy')
+player2 = PlayerCharacter('Tom')
+
+
+print(player1.name)
+print(player2.name)
